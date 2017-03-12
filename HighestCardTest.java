bf00f47418e54a3dc1fbc6b7a21a29601ba32272
kimnorth <benignhumour@gmail.com>
2017-03-12 19:48:08 +0000

    Tried implementing a mock highestcard.round but not sure I've got the hang of using it yet. The program appears to be returning either Human Wins! or Computer Wins! each time, which is the expected behaviour.

--- HighestCardTest.java
+++ HighestCardTest.java
@@ -1,30 +1,31 @@
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.*;
+import org.mockito.*;
 import static org.mockito.Mockito.*;
 
 public class HighestCardTest {
 
   HighestCard highestCard;
-  Player player1;
-  Player player2;
+  HighestCard mockHighestCard;
 
   @Before
   public void before() {
     highestCard = new HighestCard();
-    player1 = new Player("Human");
-    player2 = new Player("Computer");
+    mockHighestCard = Mockito.spy(highestCard);
   }
 
   @Test
-  public void hasWelcomeMessage(){
+  public void testHasWelcomeMessage(){
     String expected = "The rules are: highest card wins. Are you ready to play? (y/n)";
     assertEquals( expected, highestCard.welcomeMessage() );
   }
 
   @Test
-  public void hasWinner(){
+  public void testHasWinnerStub(){
     String expected = "Human wins!";
+    Mockito.when(mockHighestCard.round()).thenReturn("Human wins!");
+    String result = mockHighestCard.round();
     assertEquals( expected, highestCard.round() );
   }
 
